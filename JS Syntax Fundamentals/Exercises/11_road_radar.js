ce539395da9a270e5ba2af1357a32da859801ee9
TsvetelinaAleksandrova <123267355+TsvetelinaAleksandrova@users.noreply.github.com>
2024-03-03 21:26:57 +0200

    added task 11

---
+++ JS Syntax Fundamentals/Exercises/11_road_radar.js
@@ -0,0 +1,35 @@
+function solve(currentSpeed, area) {
+    let speedLimit = 0;
+
+    switch (area) {
+        case 'motorway':
+            speedLimit = 130;
+            break;
+        case 'interstate':
+            speedLimit = 90;
+            break;  
+        case 'city':
+            speedLimit = 50;
+            break;  
+        case 'residential':
+            speedLimit = 20;
+            break;  
+    }
+
+    const speedDifferenece = currentSpeed - speedLimit;
+
+    if (speedDifferenece <= 0) {
+        return console.log(`Driving ${currentSpeed} km/h in a ${speedLimit} zone`)
+    } 
+    
+    let status = '';
+    if (speedDifferenece <= 20) {
+        status = 'speeding';
+    } else if (speedDifferenece <= 40) {
+        status = 'excessive speeding'; 
+    } else {
+        status = 'reckless driving';
+    }
+
+    console.log(`The speed is ${speedDifferenece} km/h faster than the allowed speed of ${speedLimit} - ${status}`)
+}
