f913118009816a9e7ada205e61544d3bb67e24c1
TsvetelinaAleksandrova <123267355+TsvetelinaAleksandrova@users.noreply.github.com>
2024-03-06 22:13:32 +0200

    updated task

--- 3. Functions and Statements/Exercise/10-factorial-divison.js
+++ 3. Functions and Statements/Exercise/10-factorial-divison.js
@@ -1,9 +1,9 @@
 function solve(a, b) {
     const result = calculateFactorial(a) / calculateFactorial(b)
     console.log(result.toFixed(2));
-    
+
     function calculateFactorial(number) {
-        if (number === 1) {
+        if (number <= 1) {
             return 1;
         }
         return number * calculateFactorial(number - 1)
