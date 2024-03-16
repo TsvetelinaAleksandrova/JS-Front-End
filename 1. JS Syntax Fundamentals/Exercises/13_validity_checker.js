923d3cca64adc03fac9cb1ec6dc8f89fc047e34a
TsvetelinaAleksandrova <123267355+TsvetelinaAleksandrova@users.noreply.github.com>
2024-03-16 22:09:51 +0200

    Create 13_validity_checker.js

---
+++ 1. JS Syntax Fundamentals/Exercises/13_validity_checker.js
@@ -0,0 +1,25 @@
+function solve(x1, y1, x2, y2) {
+    firstDistance = Math.sqrt((0 - x1) ** 2 + (0 - y1) ** 2)
+    secondDistance = Math.sqrt((0 - x2) ** 2 + (0 - y2) ** 2)
+    thirdDistance = Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+
+    if (Number.isInteger(firstDistance)) {
+        console.log(`{${x1}, ${y1}} to {0, 0} is valid`)
+    } else {
+        console.log(`{${x1}, ${y1}} to {0, 0} is invalid`)
+    }
+
+    if (Number.isInteger(secondDistance)) {
+        console.log(`{${x2}, ${y2}} to {0, 0} is valid`)
+    } else {
+        console.log(`{${x2}, ${y2}} to {0, 0} is invalid`)
+    }
+
+    if (Number.isInteger(thirdDistance)) {
+        console.log(`{${x1}, ${y1}} to {${x2}, ${y2}} is valid`)
+    } else {
+        console.log(`{${x1}, ${y1}} to {${x2}, ${y2}} is invalid`)
+    }
+}
+
+solve(3, 0, 0, 4)
